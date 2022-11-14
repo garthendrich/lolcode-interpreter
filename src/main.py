8e4a78df8fb004998ec68d992b53e889670d09d9
garthendrich <garthendrich@gmail.com>
2022-11-15 01:35:56 +0800

    feat: create initial Interpreter class

--- src/main.py
+++ src/main.py
@@ -1,5 +1,42 @@
+class Interpreter:
+    def __init__(
+        self,
+    ):
+        self.symbolTable = []
+
+    def process(self, content):
+        self.tokenize(content)
+
+    def tokenize(self, content):
+        pass
+
+
 def main():
-    pass
+
+    interpreter = Interpreter()
+    interpreter.process(
+        """
+HAI 1.2
+HOW IZ I POWERTWO YR NUM
+    BTW RETURN 1 IF 2 TO POWER OF 0
+    BOTH SAEM NUM AN 0, O RLY?
+        YA RLY, FOUND YR 1
+    OIC
+
+    BTW CALCULATE 2 TO POWER OF NUM
+    I HAS A INDEX ITZ 0
+    I HAS A TOTAL ITZ 1
+    IM IN YR LOOP UPPIN YR INDEX TIL BOTH SAEM INDEX AN NUM
+        TOTAL R PRODUKT OF TOTAL AN 2
+    IM OUTTA YR LOOP
+
+    FOUND YR TOTAL
+    IF U SAY SO
+    BTW OUTPUT: 8
+    VISIBLE I IZ POWERTWO YR 4 MKAY
+KTHXBYE
+"""
+    )
 
 
 if __name__ == "__main__":
