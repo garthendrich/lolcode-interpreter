e1280ddc3ba12e99b3dbbbe89de6a5d04f43888d
garthendrich <garthendrich@gmail.com>
2022-11-16 01:43:01 +0800

    refactor: move lexer to its own module

--- src/lexer.py
+++ src/lexer.py
@@ -1,16 +1,9 @@
 import re
 
 
-class Token:
-    def __init__(self, lexeme, lexemeType):
-        self.lexeme = lexeme
-        self.lexemeType = lexemeType
-
-
-class Interpreter:
+class Lexer:
     def __init__(self):
         self.lexemes = []
-        self.symbolTable = []
 
         self.patternTypes = {
             r"^HAI$": "code delimiter",
@@ -57,10 +50,9 @@ class Interpreter:
             r"^TIL$": "[temp] placeholder",
             r"^WILE$": "[temp] placeholder",
             r"^IM OUTTA YR$": "[temp] placeholder",
-            #r"^I HAS A [a-zA-Z]\w*$": "variable identifier",
-            #r"^IM IN YR [a-zA-Z]\w*$": "loop identifier",
+            # r"^I HAS A [a-zA-Z]\w*$": "variable identifier",
+            # r"^IM IN YR [a-zA-Z]\w*$": "loop identifier",
         }
-        self.allPatterns = dict.keys(self.patternTypes)
 
     def process(self, content):
         content = self._removeIndents(content)
@@ -93,69 +85,15 @@ class Interpreter:
             #     break
 
     def _getLexemeType(self, lexeme):
-        for pattern in self.allPatterns:
-            if re.match(f"{pattern}", lexeme):
+        allPatterns = dict.keys(self.patternTypes)
+        for pattern in allPatterns:
+            if re.match(pattern, lexeme):
                 lexemeType = self.patternTypes[pattern]
                 return lexemeType
         return None
 
 
-def main():
-    interpreter = Interpreter()
-
-    interpreter.process(
-        """
-HAI
-\tI HAS A var
-\tI HAS A one ITZ 1
-\tvar R SUM OF one AN 2
-\tVISIBLE var
-
-\tI HAS A h ITZ "Meow"
-\tI HAS A dyM ITZ " "
-\tI HAS A ny ITZ "world!!" 
-\tVISIBLE SMOOSH h AN dyM AN ny
-
-\tVISIBLE BOTH OF WIN AN FAIL
-\tVISIBLE ANY OF WIN AN FAIL AN FAIL MKAY
-
-\tVISIBLE DIFFRINT 4 AN 5
-
-\tMAEK one YARN
-\tVISIBLE one
-
-\tFAIL
-\tO RLY? 
-\t\tYA RLY
-\t\t\tVISIBLE ":)"
-\t\tNO WAI
-\t\t\tVISIBLE ":("
-\tOIC
-
-\tIT R "b"
-\tWTF?
-\tOMG "a"
-\t\tVISIBLE "it's a"
-\tOMG "b"
-\t\tVISIBLE "it's b"
-\tOMG "c"
-\t\tVISIBLE "it's c"
-\tOMGWTF
-\t\tVISIBLE "it's something else"
-\tOIC
-
-\tI HAS A index ITZ 0
-\tIM IN YR hous UPPIN YR index TIL BOTH SAEM index AN 5
-\t\tVISIBLE SMOOSH "iteration " AN index
-\tIM OUTTA YR hous
-KTHXBYE
-"""
-    )
-
-    # test
-    for token in interpreter.lexemes:
-        print(f"{str.ljust(token.lexeme, 16)} {token.lexemeType}")
-
-
-if __name__ == "__main__":
-    main()
+class Token:
+    def __init__(self, lexeme, lexemeType):
+        self.lexeme = lexeme
+        self.lexemeType = lexemeType
