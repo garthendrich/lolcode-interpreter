30639d0ace3896380ca0b07efbeb4c32e0a7a79c
garthendrich <garthendrich@gmail.com>
2022-11-16 13:03:05 +0800

    fix: update patterns

--- src/lexer.py
+++ src/lexer.py
@@ -1,59 +1,218 @@
 import re
 
+
+# enum
+class STATEMENT_TYPE:
+    expression = "expression"
+    variable_identifier = "variable_identifier"
+    loop_identifier = "loop_identifier"
+    function_identifier = "function_identifier"
+
+
 class Lexer:
     def __init__(self):
         self.lexemes = []
 
-        self.patternTypes = {
-            r"^HAI$": "code delimiter",
-            r"^KTHXBYE$": "code delimiter",
-            r"^BTW$": "comment keyword",
-            r"^OBTW$": "multiline comment delimiter",
-            r"^TLDR$": "multiline comment delimiter",
-            r"^I HAS A$": "variable declaration",
-            r"^ITZ$": "variable assignment",
-            r"^R$": "variable assignment",
-            r"^SUM OF$": "arithemtic operation",
-            r"^DIFF OF$": "arithemtic operation",
-            r"^PRODUKT OF$": "arithemtic operation",
-            r"^QUOSHUNT OF$": "arithemtic operation",
-            r"^MOD OF$": "arithemtic operation",
-            r"^BIGGR OF$": "arithemtic operation",
-            r"^SMALLR$": "arithemtic operation",
-            r"^BOTH OF$": "boolean operation",
-            r"^EITHER OF$": "boolean operation",
-            r"^WON OF$": "boolean operation",
-            r"^NOT$": "boolean operation",
-            r"^ANY OF$": "boolean operation",
-            r"^ALL OF$": "boolean operation",
-            r"^BOTH SAEM$": "comparison operation",
-            r"^DIFFRINT$": "comparison operation",
-            r"^SMOOSH$": "concantenation operaion",
-            r"^MAEK$": "typecasting keyword",
-            r"^A$": "[temp] placeholder",
-            r"^IS NOW A$": "[temp] placeholder",
-            r"^VISIBLE$": "output keyword",
-            r"^GIMMEH$": "input keyword",
-            r"^O RLY\?$": "if-then statement keyword",
-            r"^YA RLY$": "flow-control statement delimeter",
-            r"^MEBBE$": "else-if statement keyword",
-            r"^NO WAI$": "[temp] placeholder",
-            r"^OIC$": "[temp] placeholder",
-            r"^WTF\?$": "[temp] placeholder",
-            r"^OMG$": "[temp] placeholder",
-            r"^OMGTWF$": "[temp] placeholder",
-            r"^IM IN YR$": "loop declaration",
-            r"^UPPIN$": "[temp] placeholder",
-            r"^NERFIN$": "[temp] placeholder",
-            r"^YR$": "[temp] placeholder",
-            r"^TIL$": "[temp] placeholder",
-            r"^WILE$": "[temp] placeholder",
-            r"^IM OUTTA YR$": "[temp] placeholder",
-            r"^I HAS A [a-zA-Z]\w*$": "variable identifier",
-            r"^IM IN YR [a-zA-Z]\w*$": "loop identifier",
-            r"^-?(\d+.\d*|\d*.\d+)$" : "float literal",
-            r"^-?\d+$" : "integer literal",
-            r"\".*\"" : "string literal",
+        self.patternsData = {
+            r"^HAI$": {
+                "lexemeType": "code delimiter",
+                "next": None,
+            },
+            r"^KTHXBYE$": {
+                "lexemeType": "code delimiter",
+                "next": None,
+            },
+            r"^BTW$": {
+                "lexemeType": "comment keyword",
+                "next": None,
+            },
+            r"^OBTW$": {
+                "lexemeType": "multiline comment delimiter",
+                "next": None,
+            },
+            r"^TLDR$": {
+                "lexemeType": "multiline comment delimiter",
+                "next": None,
+            },
+            r"^I HAS A$": {
+                "lexemeType": "variable declaration",
+                "next": STATEMENT_TYPE.variable_identifier,
+            },
+            r"^ITZ$": {
+                "lexemeType": "variable assignment",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^R$": {
+                "lexemeType": "variable assignment",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^SUM OF$": {
+                "lexemeType": "addition operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^DIFF OF$": {
+                "lexemeType": "subtraction operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^PRODUKT OF$": {
+                "lexemeType": "multiplication operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^QUOSHUNT OF$": {
+                "lexemeType": "quotient operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^MOD OF$": {
+                "lexemeType": "modulo operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^BIGGR OF$": {
+                "lexemeType": "max operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^SMALLR OF$": {
+                "lexemeType": "min operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^BOTH OF$": {
+                "lexemeType": "and operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^EITHER OF$": {
+                "lexemeType": "or operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^WON OF$": {
+                "lexemeType": "xor operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^NOT$": {
+                "lexemeType": "not operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^ANY OF$": {
+                "lexemeType": "infinite arity and operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^ALL OF$": {
+                "lexemeType": "infinite arity or operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^BOTH SAEM$": {
+                "lexemeType": "equal to operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^DIFFRINT$": {
+                "lexemeType": "not equal to operation",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^SMOOSH$": {
+                "lexemeType": "concantenation operaion",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^MAEK$": {
+                "lexemeType": "explicit typecasting keyword",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^A$": {
+                "lexemeType": "optional A keyword",
+                "next": None,
+            },
+            r"^AN$": {
+                "lexemeType": "operand seperator",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^IS NOW A$": {
+                "lexemeType": "re-casting keyword",
+                "next": None,
+            },
+            r"^VISIBLE$": {
+                "lexemeType": "output keyword",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^GIMMEH$": {
+                "lexemeType": "input keyword",
+                "next": STATEMENT_TYPE.variable_identifier,
+            },
+            r"^O RLY\?$": {
+                "lexemeType": "if-else delimeter",
+                "next": None,
+            },
+            r"^YA RLY$": {
+                "lexemeType": "if statement",
+                "next": None,
+            },
+            r"^MEBBE$": {
+                "lexemeType": "else-if statement keyword",
+                "next": None,
+            },
+            r"^NO WAI$": {
+                "lexemeType": "else statement keyword",
+                "next": None,
+            },
+            r"^OIC$": {
+                "lexemeType": "flow-control statements delimeter",
+                "next": None,
+            },
+            r"^WTF\?$": {
+                "lexemeType": "switch-case statement delimeter",
+                "next": None,
+            },
+            r"^OMG$": {
+                "lexemeType": "case keyword",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^OMGTWF$": {
+                "lexemeType": "default case keyword",
+                "next": None,
+            },
+            r"^IM IN YR$": {
+                "lexemeType": "loop declaration and delimeter",
+                "next": STATEMENT_TYPE.loop_identifier,
+            },
+            r"^UPPIN$": {
+                "lexemeType": "increment keyword",
+                "next": None,
+            },
+            r"^NERFIN$": {
+                "lexemeType": "decrement keyword",
+                "next": None,
+            },
+            r"^YR$": {
+                "lexemeType": "keyword in loop",
+                "next": STATEMENT_TYPE.variable_identifier,
+            },
+            r"^TIL$": {
+                "lexemeType": "loop condition keyword",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^WILE$": {
+                "lexemeType": "loop condition keyword",
+                "next": STATEMENT_TYPE.expression,
+            },
+            r"^IM OUTTA YR$": {
+                "lexemeType": "loop delimiter",
+                "next": None,
+            },
+        }
+
+        self.literalPatternsData = {
+            r"^-?\d*.\d+$": {
+                "lexemeType": "float literal",
+                "next": None,
+            },
+            r"^-?\d+$": {
+                "lexemeType": "integer literal",
+                "next": None,
+            },
+            r"^\".*\"$": {
+                "lexemeType": "string literal",
+                "next": None,
+            },
+            r"^(WIN|FAIL)$": {
+                "lexemeType": "bool literal",
+                "next": None,
+            },
         }
 
     def process(self, content):
