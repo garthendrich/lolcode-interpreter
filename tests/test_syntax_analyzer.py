994b7b8e5a3d10951ef83031805517e86772441b
garthendrich <garthendrich@gmail.com>
2022-12-01 09:00:29 +0800

    test(parser): add loop tests

--- tests/test_syntax_analyzer.py
+++ tests/test_syntax_analyzer.py
@@ -2,6 +2,7 @@ import unittest
 from src.components.lexer import Lexer
 from src.components.syntax_analyzer import Parser
 
+
 lexer = Lexer()
 parser = Parser()
 
@@ -13,7 +14,10 @@ class TestProgramAbstraction(unittest.TestCase):
         KTHXBYE"""
         )
 
-        parser.parse(lexemes)
+        try:
+            parser.parse(lexemes)
+        except SyntaxError:
+            self.fail("Unexpected syntax error")
 
     def test_no_hai(self):
         lexemes = lexer.process("KTHXBYE")
@@ -48,3 +52,19 @@ class TestProgramAbstraction(unittest.TestCase):
 
         with self.assertRaises(SyntaxError):
             parser.parse(lexemes)
+
+
+class TestLoopAbstraction(unittest.TestCase):
+    def test_loop(self):
+        lexemes = lexer.process(
+            """HAI
+            IM IN YR asc UPPIN YR num2 WILE BOTH SAEM num2 AN SMALLR OF num2 AN num1
+                VISIBLE num2
+            IM OUTTA YR asc
+        KTHXBYE"""
+        )
+
+        try:
+            parser.parse(lexemes)
+        except SyntaxError:
+            self.fail("Unexpected syntax error")
